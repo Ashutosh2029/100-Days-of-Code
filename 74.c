68645ea7be22595057f535486c9cf838b3432631
Ashutosh2029 <shrivastavaashutosh23@gmail.com>
2025-11-27 23:55:20 +0530

    Implement matrix transpose functionality 74.c

---
+++ 74.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    // Input rows and columns
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+    int transpose[cols][rows];
+
+    // Read matrix elements
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Compute transpose
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    // Print transpose
+    for (i = 0; i < cols; i++) {
+        for (j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
